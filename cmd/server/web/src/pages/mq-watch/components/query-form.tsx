e889aa1a8753ab4ffdf36b126dbe70360b0478d2
zcubbs <zackcubby@gmail.com>
2023-11-01 15:44:33 +0100

    :rocket: wip: build prototype

--- cmd/server/web/src/pages/mq-watch/components/query-form.tsx
+++ cmd/server/web/src/pages/mq-watch/components/query-form.tsx
@@ -1,24 +1,60 @@
-import React, {useState} from 'react';
-import {fetchData} from "@/pages/mq-watch/api.ts";
+"use client"
 
-const QueryForm: React.FC = () => {
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [data, setData] = useState(null);
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import * as z from "zod"
 
-  const handleQuery = async () => {
-    try {
-      const result = await fetchData(startDate, endDate);
-      setData(result);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
+import {cn} from "@/lib/utils"
+import {Button} from "@/components/ui/button"
+import {Form, FormControl, FormField, FormItem, FormMessage,} from "@/components/ui/form"
+import {toast} from "@/components/ui/use-toast"
+import {DatePickerWithRange} from "@/components/date-range-picker.tsx";
+
+const FormSchema = z.object({
+  range: z.string({
+    required_error: "A date range is required.",
+  }),
+})
+
+export function DatePickerForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data)
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
 
   return (
-    <div className="query-form-container">
-    </div>
-  );
-};
-
-export default QueryForm;
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="range"
+          render={({field}) => (
+            <FormItem className="flex flex-col">
+              <FormControl>
+                <DatePickerWithRange
+                  className={cn(
+                    "w-[240px] pl-3 text-left font-normal",
+                    !field.value && "text-muted-foreground"
+                  )}
+                />
+              </FormControl>
+              <FormMessage/>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
--- cmd/server/web/src/pages/mq-watch/page.tsx
+++ cmd/server/web/src/pages/mq-watch/page.tsx
@@ -1,9 +1,12 @@
 import MessagesLineChart from "@/pages/mq-watch/components/chart-v2.tsx";
+import {DatePickerWithRange} from "@/components/date-range-picker.tsx";
+import {DatePickerForm} from "@/pages/mq-watch/components/query-form.tsx";
 
 function MQWatchPage() {
   return (
     <>
       {/*<QueryForm />*/}
+      <DatePickerForm />
       <MessagesLineChart />
       {/*<MessagesChart tenantMessages={chartDataMessages} />*/}
       {/*<Graph data={fetchData} />*/}
