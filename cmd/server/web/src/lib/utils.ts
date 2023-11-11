d881a93c2bfe340b26c3d29e7123030b26276b32
zcubbs <zackcubby@gmail.com>
2023-11-11 08:14:59 +0100

    :rocket: wip: multi-topic support

--- cmd/server/web/src/lib/utils.ts
+++ cmd/server/web/src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
+export const API_URL = import.meta.env.VITE_API_URL;
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
--- cmd/server/web/src/pages/mq-watch/page.tsx
+++ cmd/server/web/src/pages/mq-watch/page.tsx
@@ -1,16 +1,11 @@
 import MessagesLineChart from "@/pages/mq-watch/components/chart-v2.tsx";
-import {DatePickerWithRange} from "@/components/date-range-picker.tsx";
 import {DatePickerForm} from "@/pages/mq-watch/components/query-form.tsx";
 
 function MQWatchPage() {
   return (
     <>
-      {/*<QueryForm />*/}
       <DatePickerForm />
       <MessagesLineChart />
-      {/*<MessagesChart tenantMessages={chartDataMessages} />*/}
-      {/*<Graph data={fetchData} />*/}
-      {/*<MessageStatsList data={statData}/>*/}
     </>
   );
 }
